298564652638886e1b5f2333396157c4e16844b4
Nathan Pannell <n8tewolf@gmail.com>
2025-10-19 14:02:11 -0700

    feat: fixed bot :)

--- discord/example_bot.py
+++ discord/example_bot.py
@@ -2,6 +2,9 @@
 # This example requires the 'message_content' intent.
 
 import asyncio
+import datetime
+from datetime import time
+
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -11,47 +14,61 @@ import requests
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+ECHO_API_URL = "http://127.0.0.1:8000/"
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-def send_message_to_db(message):
-    # Placeholder function to simulate sending a message to a database
-    payload = {
-        "user": str(message.author.id),
-        "user_name": str(message.author.name),
-        "timestamp": str(message.created_at),
-        "content": message.content
+def post_user_message(content, user_id):
+    entry_payload = {
+        "discordId": user_id,
+        "timestamp": str(datetime.datetime.now()),
+        "content": content,
+        "role": "user",
+        "notes": None
     }
-    # response = requests.post("http://your-database-endpoint", json=payload)
+    print(entry_payload)
 
+    requests.post(ECHO_API_URL + "entries", json=entry_payload)
 
-async def start_timer(seconds, channel):
-    await asyncio.sleep(seconds)
-    print("Timer finished")
-    await channel.send('Please add another log entry!')
+def post_bot_message(content, user_id):
+    entry_payload = {
+        "discordId": user_id,
+        "timestamp": str(datetime.datetime.now()),
+        "content": content,
+        "role": "bot",
+        "notes": None
+    }
+    print(entry_payload)
 
-    
+    requests.post(ECHO_API_URL + "entries", json=entry_payload)
+
+async def send_message_to_channel(message, user_id, channel, sleep_in_seconds=0):
+    await asyncio.sleep(sleep_in_seconds)
+    post_bot_message(message, user_id)
+    await channel.send(message)
+
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
 @bot.event
 async def on_message(message):
+    # Return early if the message is from the bot
     if message.author == bot.user:
         return
 
+    user_id = str(message.author.id)
     if message.content.startswith("!"):
         await bot.process_commands(message)
     else:
-        await message.channel.send('Log received!')
-        send_message_to_db(message)
+        post_user_message(message.content, user_id)
 
-        await start_timer(10, message.channel)
-
-
+        bot_response = "Log received!"
+        await send_message_to_channel(bot_response, user_id, message.channel)
+        await send_message_to_channel("Timeout complete, send another message", user_id, message.channel, 10)
 
 @bot.command()
 async def hello(ctx):
